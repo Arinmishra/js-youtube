0a06fd130634525ef78d8eaa802a54e98d46c731
Arin Mishra <126757186+Arinmishra@users.noreply.github.com>
2023-11-10 18:20:42 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 23155
+let accountName = "Arin"
+var password = "2345566"
+let accountHolder;
+accountNumber = 23566
+
+
+accountName = "Mishra"
+
+console.log(accountId);
+
+console.table([accountId, accountName, password,accountHolder, accountNumber])
